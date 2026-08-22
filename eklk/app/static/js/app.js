bc88c58045625e3004de7772a99619fd575a2faa
Nikita83USSR <nikita@ecomkassa.ru>
2026-08-22 19:46:24 +0000

    fix: OP_LABELS for correction operations

--- eklk/app/static/js/app.js
+++ eklk/app/static/js/app.js
@@ -90,6 +90,10 @@
     sell_refund: "Возврат прихода",
     buy: "Расход",
     buy_refund: "Возврат расхода",
+    sell_correction: "Коррекция прихода",
+    buy_correction: "Коррекция расхода",
+    sell_refund_correction: "Коррекция возврата прихода",
+    buy_refund_correction: "Коррекция возврата расхода",
   };
   const SNO_LABELS = {
     osn: "ОСН",
